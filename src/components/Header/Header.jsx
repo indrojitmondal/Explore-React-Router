cb0a8c25ad6e777b633ba7e4d1845fad081384f6
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-30 19:05:42 +0600

    added useNavigation and useLocation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css'
 const Header = () => {
     return (
         <div>
             <h2>Navbar</h2>
-           <div className='flex gap-4'>
-             <Link to='/' >Home</Link>
-             <Link to='/users'>Users</Link>
+           <nav className='flex gap-4'>
+            <NavLink to='/'>Home </NavLink>
+             {/* <Link to='/' >Home</Link> */}
+             {/* <Link to='/users'>Users</Link> */}
+             <NavLink to='/users'>Users </NavLink>
 
-             <Link to='/posts'>Posts</Link>
+             {/* <Link to='/posts'>Posts</Link> */}
+             <NavLink to='/posts'>Posts </NavLink>
           
-            <Link to='/about' >About</Link>
-            <Link to='/contact' >Contact US</Link>
+            {/* <Link to='/about' >About</Link> */}
+            <NavLink to='/about'>About </NavLink> 
+            {/* <Link to='/contact' >Contact US</Link> */}
+            <NavLink to='/contact'>Contact US </NavLink> 
             
-           </div>
+            
+           </nav>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location);
     return (
         <div>
             <Header></Header>
-            <h2>This is the home component</h2>
-            <Outlet></Outlet>
+            {
+                navigation.state ==='loading' ? <p>Loading</p> : 
+                <Outlet></Outlet>
+            }
+            {/* <h2>This is the home component</h2> */}
+        
            
             <Footer></Footer>
             
